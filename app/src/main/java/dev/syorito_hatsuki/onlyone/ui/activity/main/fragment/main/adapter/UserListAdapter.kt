9c45bf14246b8d53df26bb1683f88850a1e91eea
Katelinlis <katelinlis1999@gmail.com>
2022-02-25 14:02:46 +0300

    Add grid layout and on click event

--- app/src/main/java/dev/syorito_hatsuki/onlyone/ui/activity/main/fragment/main/adapter/UserListAdapter.kt
+++ app/src/main/java/dev/syorito_hatsuki/onlyone/ui/activity/main/fragment/main/adapter/UserListAdapter.kt
@@ -1,28 +1,52 @@
 package dev.syorito_hatsuki.onlyone.ui.activity.main.fragment.main.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
 import dev.syorito_hatsuki.onlyone.R
 import dev.syorito_hatsuki.onlyone.api.user.get_users.response.User
+import dev.syorito_hatsuki.onlyone.databinding.ActivityMainBinding
 import dev.syorito_hatsuki.onlyone.databinding.ItemUserBinding
+import java.text.FieldPosition
 
-class UserListAdapter(private val userList: List<User>) :
-    RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
-    class UserViewHolder(private val item: ItemUserBinding) : RecyclerView.ViewHolder(item.root) {
+class UserListAdapter(private val userList: List<User>) : RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
+
+    private lateinit var mListener: OnItemClickListener
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener){
+        mListener = listener
+    }
+
+    class UserViewHolder(item: View,listener: OnItemClickListener) : RecyclerView.ViewHolder(item) {
+        val binding = ItemUserBinding.bind(item)
+
+        init {
+            item.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
+
         fun bind(id: Int, avatar: String, username: String) {
-            item.userId.text = id.toString()
-            item.userAvatar.load("https://cdn.only-one.su/public/clients/$id/$avatar") {
+            //item.userId.text = id.toString()
+
+            binding.Image.load("https://cdn.only-one.su/public/clients/$id/$avatar") {
                 error(R.drawable.no_avatar)
                 transformations(CircleCropTransformation())
             }
-            item.username.text = username
+            binding.Username.text = username
         }
+
     }
 
-    override fun onCreateViewHolder(
+   /* override fun onCreateViewHolder(
         viewGroup: ViewGroup,
         viewType: Int
     ): UserViewHolder = UserViewHolder(
@@ -31,7 +55,12 @@ class UserListAdapter(private val userList: List<User>) :
             viewGroup,
             false
         )
-    )
+    )*/
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false)
+        return UserViewHolder(view,mListener)
+    }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val user = userList[position]
@@ -39,4 +68,6 @@ class UserListAdapter(private val userList: List<User>) :
     }
 
     override fun getItemCount(): Int = userList.size
+
+
 }
--- app/src/main/java/dev/syorito_hatsuki/onlyone/ui/activity/main/fragment/main/view/MainFragment.kt
+++ app/src/main/java/dev/syorito_hatsuki/onlyone/ui/activity/main/fragment/main/view/MainFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import dev.syorito_hatsuki.onlyone.databinding.ActivityMainBinding
 import dev.syorito_hatsuki.onlyone.databinding.FragmentMainBinding
 import dev.syorito_hatsuki.onlyone.ui.activity.main.fragment.main.adapter.UserListAdapter
 import dev.syorito_hatsuki.onlyone.ui.activity.main.fragment.main.viewmodel.MainViewModel
 import kotlinx.coroutines.flow.collect
 
-class MainFragment : Fragment(), LifecycleObserver {
+class MainFragment : Fragment(), LifecycleObserver  {
 
     private val viewModel: MainViewModel by viewModels()
     private lateinit var viewBinding: FragmentMainBinding
@@ -26,11 +29,25 @@ class MainFragment : Fragment(), LifecycleObserver {
         return viewBinding.root
     }
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         lifecycleScope.launchWhenCreated {
             viewModel.getUsersList().collect {
-                viewBinding.recycler.adapter = UserListAdapter(it.users)
+
+                    viewBinding.recycler.layoutManager = GridLayoutManager(requireContext(),3)
+                    val adapter = UserListAdapter(it.users)
+
+                    viewBinding.recycler.adapter = adapter
+
+                adapter.setOnItemClickListener(object : UserListAdapter.OnItemClickListener{
+                    override fun onItemClick(position: Int) {
+                        Toast.makeText(requireContext(),"hello ${it.users[position].username}",Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+
             }
         }
     }
